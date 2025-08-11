92f2dfc2ea89d3ae17f4835e0f934b7f5879cf89
Irshad,Sumair <sirshad@mtwsolutions.com>
2025-08-11 19:39:17 +0500

    get favourited properties

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,30 +1,47 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import PropertyCard from "../components/propertyCard";
+import { fetchIsPropertyFavourited } from "../Services/BaseAPI";
+import { useUser } from "../context/userContext";
+import { APIProperty, PropertyCardData } from "../sharedProperty";
 
-const favoriteProperties = [
-  {
-    id: 3,
-    title: "Beachside Villa",
-    price: 1200000,
-    address: "78 Ocean Rd, Gold Coast",
-    bedrooms: 5,
-    bathrooms: 3,
-    image: "https://w0.peakpx.com/wallpaper/665/714/HD-wallpaper-a-house-is-not-a-home-place-home-house-zynah.jpg",
-  },
-  {
-    id: 4,
-    title: "Urban Loft",
-    price: 610000,
-    address: "12 City Lane, Brisbane",
-    bedrooms: 2,
-    bathrooms: 2,
-    image: "https://i.pinimg.com/736x/93/e1/0e/93e10e06e28a305bbb1f9be260cec04f.jpg",
-  },
-];
 
 export default function FavoritesPage() {
+  const {userId} = useUser();
+  const [properties, setProperties] = useState<PropertyCardData[]>([]);
+
+  
+useEffect(() => {
+  if(!userId) return;
+  const load = async () => {
+
+    try {
+      const res = await fetchIsPropertyFavourited<{
+        result: APIProperty[];
+        isSuccess: boolean;
+        messages: string[];
+        errorMessages?: string[];
+      }>({userId});
+
+      if (res.isSuccess) {
+        const formatted: PropertyCardData[] = res.result.map((prop) => ({
+          ...prop,
+          listingType: prop.listingType === 1 ? "rent" : "sell",
+        }));
+        setProperties(formatted);
+      } else {
+        console.error(res.errorMessages?.[0] || "Failed to fetch.");
+      }
+    } catch (err) {
+      console.error("Unexpected error:", err);
+    }
+  };
+
+  load();
+}, [userId]);
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
@@ -32,12 +49,12 @@ export default function FavoritesPage() {
       <div className="max-w-7xl mx-auto p-6 sm:p-10">
         <h1 className="text-2xl font-bold text-gray-800 mb-6">My Favorite Properties</h1>
 
-        {favoriteProperties.length === 0 ? (
+        {properties.length === 0 ? (
           <p className="text-gray-500">You have no favorite properties yet.</p>
         ) : (
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-            {favoriteProperties.map((prop) => (
-              <PropertyCard key={prop.id} {...prop} />
+            {properties.map((prop,index) => (
+              <PropertyCard key={index} {...prop} />
             ))}
           </div>
         )}
