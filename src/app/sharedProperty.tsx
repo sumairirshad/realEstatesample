1d1551cd313091d5435a1d9ae657aab7f7c19984
Irshad,Sumair <sirshad@mtwsolutions.com>
2025-08-11 19:38:19 +0500

    add properties

--- src/app/sharedProperty.tsx
+++ src/app/sharedProperty.tsx
@@ -1,33 +1,21 @@
 
 
-// export const properties: {
-//   id: number;
-//   title: string;
-//   price: number;
-//   address: string;
-//   bedrooms: number;
-//   bathrooms: number;
-//   image: string;
-//   listingType: ListingType; 
-// }[] = [
-//   {
-//     id: 1,
-//     title: "Modern Apartment",
-//     price: 1200,
-//     address: "123 Main St, NY",
-//     bedrooms: 2,
-//     bathrooms: 1,
-//     image: "/images/apt1.jpg",
-//     listingType: "rent", 
-//   },
-//   {
-//     id: 2,
-//     title: "Luxury Villa",
-//     price: 850000,
-//     address: "789 Sunset Blvd, LA",
-//     bedrooms: 5,
-//     bathrooms: 4,
-//     image: "/images/villa.jpg",
-//     listingType: "sell", // ✅
-//   },
-// ];
+export type ListingType = "rent" | "sell"; 
+
+export type APIProperty = {
+  id: number;
+  title: string;
+  price: number;
+  address: string;
+  bedrooms: number;
+  bathrooms: number;
+  carspots: number;
+  imageUrl: string;
+  listingType: number; 
+  isFavourite: boolean;
+  descritption:string;
+};
+
+export type PropertyCardData = Omit<APIProperty, "listingType"> & {
+  listingType: ListingType; 
+}
