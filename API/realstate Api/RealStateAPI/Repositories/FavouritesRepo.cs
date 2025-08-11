82da400749ed147f245955281e23ddaa307c05d5
Irshad,Sumair <sirshad@mtwsolutions.com>
2025-08-11 19:41:04 +0500

    implement to get favourate propetries

--- API/realstate Api/RealStateAPI/Repositories/FavouritesRepo.cs
+++ API/realstate Api/RealStateAPI/Repositories/FavouritesRepo.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using RealStateAPI.AppVars;
 using RealStateAPI.Context;
 using RealStateAPI.DTO;
@@ -38,9 +39,31 @@ namespace RealStateAPI.Repositories
             return (newFav, true);
         }
 
-        public async Task<IList<Favourites>> FetchFavouritedProperties(int userId)
+        public async Task<IList<dynamic>> FetchFavouritedProperties(int userId)
         {
-            return await _context.Favourites.Where(w => w.UserId == userId).ToListAsync();
+            var result = await (from fav in _context.Favourites
+                                join prop in _context.Properties
+                                on fav.PropertyId equals prop.Id
+                                where fav.UserId == userId
+                                select new
+                                {
+                                    prop.Id,
+                                    prop.UserId,
+                                    prop.Title,
+                                    prop.Descritption,
+                                    prop.Address,
+                                    prop.listingType,
+                                    prop.Bedrooms,
+                                    prop.Bathrooms,
+                                    prop.Carspots,
+                                    prop.ImageUrl,
+                                    prop.CreatedAt,
+                                    prop.Price,
+                                    IsFavourite = userId != null &&
+                                                      _context.Favourites.Any(f => f.PropertyId == prop.Id && f.UserId == userId)
+                                }).ToListAsync<dynamic>(); 
+
+            return result;
         }
     }
 }
